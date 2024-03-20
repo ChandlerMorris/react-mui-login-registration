f0b065dbefd06197b40ac5a6a426ef7151ce1a05
ChandlerMorris <150638881+ChandlerMorris@users.noreply.github.com>
2024-03-19 22:48:54 -0400

    Wrapped forms with box and removed padding for password field adornment

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
-import { Button, FormControl, IconButton, InputAdornment, Link, Stack, TextField, Typography } from "@mui/material";
+import { Box, Button, FormControl, IconButton, InputAdornment, Link, Stack, TextField, Typography } from "@mui/material";
 
 const Login = () => {
     const [email, setEmail] = useState<String>('');
@@ -9,50 +9,55 @@ const Login = () => {
     const handleShowPassword = () => setShowPassword(!showPassword);
 
     return (
-        <FormControl sx={{bgcolor: 'primary.main', color: 'secondary.main', padding: '8rem 6rem', border: '1px solid #000', textAlign: 'center'}}>
-            <Typography variant="h2">Sign In</Typography>
-            <Stack spacing={2} sx={{margin: '1rem 0'}}>
-                <TextField 
-                    id="email" 
-                    label="Email" 
-                    variant="outlined" 
-                    color="secondary" 
-                    size="small" 
-                    value={email} 
-                    onChange={e => setEmail(e.target.value)}
-                    type="text" 
-                    required
-                />
-                <TextField 
-                    id="password" 
-                    label="Password" 
-                    variant="outlined" 
-                    color="secondary" 
-                    size="small" 
-                    value={password} 
-                    onChange={e => setPassword(e.target.value)} 
-                    type={showPassword ? "text" : "password"}
-                    InputProps={{
-                        endAdornment: (
-                            <InputAdornment position="end">
-                                <IconButton
-                                    aria-label="toggle password visibility"
-                                    onClick={handleShowPassword}
-                                >
-                                    {showPassword ? <Visibility /> : <VisibilityOff />}
-                                </IconButton>
-                            </InputAdornment>
-                        )
-                    }}
-                    required
-                />
-                <Button variant="outlined" color="secondary">Sign In</Button>
-            </Stack>
-            <Stack spacing={1} direction="row" sx={{marginBottom: '1rem', justifyContent: "center"}}>
-                <Typography variant="subtitle1">Don't have an account?</Typography>
-                <Link href="/register" sx={{color: 'secondary.main', textDecorationColor: 'inherit'}}>Create an account</Link>
-            </Stack>
-        </FormControl>
+        <Box sx={{ width: 500}}>
+            <FormControl sx={{bgcolor: 'primary.main', color: 'secondary.main', padding: '8rem 6rem', border: '1px solid #000', textAlign: 'center'}}>
+                <Typography variant="h2">Sign In</Typography>
+                <Stack spacing={2} sx={{margin: '1rem 0'}}>
+                    <TextField 
+                        id="email" 
+                        label="Email" 
+                        variant="outlined" 
+                        color="secondary" 
+                        size="small" 
+                        value={email} 
+                        onChange={e => setEmail(e.target.value)}
+                        type="text" 
+                        required
+                    />
+                    <TextField 
+                        id="password" 
+                        label="Password" 
+                        variant="outlined" 
+                        color="secondary" 
+                        size="small" 
+                        value={password} 
+                        onChange={e => setPassword(e.target.value)} 
+                        type={showPassword ? "text" : "password"}
+                        InputProps={{
+                            style: {
+                                paddingRight: 2
+                            },
+                            endAdornment: (
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        aria-label="toggle password visibility"
+                                        onClick={handleShowPassword}
+                                    >
+                                        {showPassword ? <Visibility /> : <VisibilityOff />}
+                                    </IconButton>
+                                </InputAdornment>
+                            )
+                        }}
+                        required
+                    />
+                    <Button variant="outlined" color="secondary">Sign In</Button>
+                </Stack>
+                <Stack spacing={1} direction="row" sx={{marginBottom: '1rem', justifyContent: "center"}}>
+                    <Typography variant="subtitle1">Don't have an account?</Typography>
+                    <Link href="/register" sx={{color: 'secondary.main', textDecorationColor: 'inherit'}}>Create an account</Link>
+                </Stack>
+            </FormControl>
+        </Box>
     )
 }
 
--- src/components/register/Register.tsx
+++ src/components/register/Register.tsx
@@ -1,6 +1,6 @@
 import { SetStateAction, useState } from "react";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
-import { Button, FormControl, IconButton, InputAdornment, Link, Stack, TextField, Typography } from "@mui/material";
+import { Box, Button, FormControl, IconButton, InputAdornment, Link, Stack, TextField, Typography } from "@mui/material";
 import dayjs, { Dayjs } from 'dayjs';
 import DateInput from "../dateInput/DateInput";
 
@@ -15,75 +15,81 @@ const Register = () => {
     const handleShowPassword = () => setShowPassword(!showPassword);
 
     return (
-        <FormControl sx={{bgcolor: 'primary.main', color: 'secondary.main', padding: '8rem 6rem', border: '1px solid #000', textAlign: 'center'}}>
-            <Typography variant="h2">Sign Up</Typography>
-            <Stack spacing={2} sx={{margin: '1rem 0'}}>
-                <Stack spacing={2} direction="row" sx={{margin: '1rem 0'}}>
+        <Box sx={{ width: 500}}>
+            <FormControl sx={{bgcolor: 'primary.main', color: 'secondary.main', padding: '8rem 6rem', border: '1px solid #000', textAlign: 'center'}}>
+                <Typography variant="h2">Sign Up</Typography>
+                <Stack spacing={2} sx={{margin: '1rem 0'}}>
+                    <Stack spacing={2} direction="row" sx={{margin: '1rem 0'}}>
+                        <TextField 
+                            id="firstName" 
+                            label="First Name" 
+                            variant="outlined" 
+                            color="secondary" 
+                            size="small" 
+                            value={firstName} 
+                            onChange={e => setFirstName(e.target.value)}
+                            type="text" 
+                            required
+                        />
+                        <TextField 
+                            id="lastName" 
+                            label="Last Name" 
+                            variant="outlined" 
+                            color="secondary" 
+                            size="small" 
+                            value={lastName} 
+                            onChange={e => setLastName(e.target.value)}
+                            type="text" 
+                            required
+                        />
+                    </Stack>
                     <TextField 
-                        id="firstName" 
-                        label="First Name" 
+                        id="email" 
+                        label="Email" 
                         variant="outlined" 
                         color="secondary" 
                         size="small" 
-                        value={firstName} 
-                        onChange={e => setFirstName(e.target.value)}
+                        value={email} 
+                        onChange={e => setEmail(e.target.value)}
                         type="text" 
                         required
                     />
                     <TextField 
-                        id="lastName" 
-                        label="Last Name" 
+                        id="password" 
+                        label="Password" 
                         variant="outlined" 
                         color="secondary" 
                         size="small" 
-                        value={lastName} 
-                        onChange={e => setLastName(e.target.value)}
-                        type="text" 
+                        value={password} 
+                        onChange={e => setPassword(e.target.value)} 
+                        type={showPassword ? "text" : "password"}
+                        sx={{ paddingRight: 0}}
+                        InputProps={{
+                            style: {
+                                paddingRight: 2
+                            },
+                            endAdornment: (
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        aria-label="toggle password visibility"
+                                        onClick={handleShowPassword}
+                                    >
+                                        {showPassword ? <Visibility /> : <VisibilityOff />}
+                                    </IconButton>
+                                </InputAdornment>
+                            )
+                        }}
                         required
                     />
+                    <DateInput label="Date of Birth (MM/DD/YYYY)" date={birthday} setDate={handleUpdateBirthday} />
+                    <Button variant="outlined" color="secondary">Sign Up</Button>
                 </Stack>
-                <TextField 
-                    id="email" 
-                    label="Email" 
-                    variant="outlined" 
-                    color="secondary" 
-                    size="small" 
-                    value={email} 
-                    onChange={e => setEmail(e.target.value)}
-                    type="text" 
-                    required
-                />
-                <TextField 
-                    id="password" 
-                    label="Password" 
-                    variant="outlined" 
-                    color="secondary" 
-                    size="small" 
-                    value={password} 
-                    onChange={e => setPassword(e.target.value)} 
-                    type={showPassword ? "text" : "password"}
-                    InputProps={{
-                        endAdornment: (
-                            <InputAdornment position="end">
-                                <IconButton
-                                    aria-label="toggle password visibility"
-                                    onClick={handleShowPassword}
-                                >
-                                    {showPassword ? <Visibility /> : <VisibilityOff />}
-                                </IconButton>
-                            </InputAdornment>
-                        )
-                    }}
-                    required
-                />
-                <DateInput label="Date of Birth (MM/DD/YYYY)" date={birthday} setDate={handleUpdateBirthday} />
-                <Button variant="outlined" color="secondary">Sign Up</Button>
-            </Stack>
-            <Stack spacing={1} direction="row" sx={{marginBottom: '1rem', justifyContent: "center"}}>
-                <Typography variant="subtitle1">Already have an account?</Typography>
-                <Link href="/" sx={{color: 'secondary.main', textDecorationColor: 'inherit'}}>Create an account</Link>
-            </Stack>
-        </FormControl>
+                <Stack spacing={1} direction="row" sx={{marginBottom: '1rem', justifyContent: "center"}}>
+                    <Typography variant="subtitle1">Already have an account?</Typography>
+                    <Link href="/" sx={{color: 'secondary.main', textDecorationColor: 'inherit'}}>Sign In</Link>
+                </Stack>
+            </FormControl>
+        </Box>
     )
 }
 
