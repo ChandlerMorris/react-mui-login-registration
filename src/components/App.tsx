89bb33d8e7f48b9082fb0b79556d498341a2f6eb
ChandlerMorris <150638881+ChandlerMorris@users.noreply.github.com>
2024-03-19 22:48:54 -0400

    State added to input fields for username and password

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,10 @@
+import { Container } from "@mui/material";
 import Login from "./login/Login";
 
 export default function App() {
     return (
-        <>
+        <Container fixed sx={{height: "100vh", display: "flex", justifyContent: "center", alignItems: "center"}}>
             <Login />
-        </>
+        </Container>
     )
 }
--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,12 +1,49 @@
-import { Button, FormControl, Link, Stack, TextField, Typography } from "@mui/material";
+import { useState } from "react";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import { Button, FormControl, IconButton, InputAdornment, Link, Stack, TextField, Typography } from "@mui/material";
 
 export default function Login() {
+    const [username, setUsername] = useState<String>('');
+    const [password, setPassword] = useState<String>('');
+    const [showPassword, setShowPassword] = useState<boolean>(false);
+    const handleShowPassword = () => setShowPassword(!showPassword);
+
     return (
         <FormControl sx={{bgcolor: 'primary.main', color: 'secondary.main', padding: '8rem 6rem', border: '1px solid #000', textAlign: 'center'}}>
             <Typography variant="h2">Sign In</Typography>
             <Stack spacing={2} sx={{margin: '1rem 0'}}>
-                <TextField id="outlined-basic" label="Name" variant="outlined" color="secondary" size="small" />
-                <TextField id="outlined-basic" label="Email" variant="outlined" color="secondary" size="small" />
+                <TextField 
+                    id="username" 
+                    label="Username" 
+                    variant="outlined" 
+                    color="secondary" 
+                    size="small" 
+                    value={username} 
+                    onChange={e => setUsername(e.target.value)}
+                    type="text" 
+                />
+                <TextField 
+                    id="password" 
+                    label="Password" 
+                    variant="outlined" 
+                    color="secondary" 
+                    size="small" 
+                    value={password} 
+                    onChange={e => setPassword(e.target.value)} 
+                    type={showPassword ? "text" : "password"}
+                    InputProps={{
+                        endAdornment: (
+                            <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={handleShowPassword}
+                                >
+                                    {showPassword ? <Visibility /> : <VisibilityOff />}
+                                </IconButton>
+                            </InputAdornment>
+                        )
+                    }}
+                />
                 <Button variant="outlined" color="secondary">Sign In</Button>
             </Stack>
             <Stack spacing={1} direction="row" sx={{marginBottom: '1rem'}}>
